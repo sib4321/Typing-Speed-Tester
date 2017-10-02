f61f517f55258e665d41a171456f474ff0840f29
sib4321 <sib4321@ukr.net>
2017-10-02 17:19:51 +0300

    Stop the clock when the test is done

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 var timer = [0, 0, 0, 0];
+var interval;
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -31,6 +33,7 @@ function spellCheck() {
     let originTextMatch = originText.substring(0, textEntered.length);
 
     if (textEntered == originText) {
+        clearInterval(interval);
         testWrapper.style.borderColor = "#429890";
     } else {
         if (textEntered == originTextMatch) {
@@ -44,8 +47,9 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnteredLength = testArea.value.length;
-    if (textEnteredLength === 0) {
-        setInterval(runTimer, 10);
+    if (textEnteredLength === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
     }
     console.log(textEnteredLength);
 }
