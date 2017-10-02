56f0f75b5892d587fda90bea24b0a4c645c3f44c
sib4321 <sib4321@ukr.net>
2017-10-02 17:26:27 +0300

    Add a reset button

--- script.js
+++ script.js
@@ -56,7 +56,14 @@ function start() {
 
 // Reset everything:
 function reset() {
-    console.log("reset button has been pressed");
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timerRunning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
 }
 
 // Event listeners for keyboard input and the reset button:
